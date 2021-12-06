0633198ee8da275d3b3800a40f02ec287f301d95
SergioEvando23 <sergioevandocosta@gmail.com>
2021-12-05 22:48:39 -0300

    Sergio Requisito 18: Adicionado validação do checkbox para ativar o botão submit

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const inputPassHeader = document.getElementById('input-pass-header');
 const submitLogin = document.getElementById('button-submit-login');
 const validEmail = 'tryber@teste.com';
 const validPass = '123456';
+const buttonSendForm = document.getElementById('submit-btn');
+const authorization = document.getElementById('agreement');
 
 submitLogin.onclick = () => {
   if (inputEmailHeader.value === validEmail && inputPassHeader.value === validPass) {
@@ -11,3 +13,10 @@ submitLogin.onclick = () => {
     alert('Email ou senha inválidos.');
   }
 };
+authorization.addEventListener('click', () => {
+  if (authorization.checked === true) {
+    buttonSendForm.disabled = false;
+  } else {
+    buttonSendForm.disabled = true;
+  }
+});
